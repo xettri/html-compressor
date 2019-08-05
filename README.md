b6e7cf6b84140d7d3b260605cbde91a16ca96db9
BCrazyDreamer <bcrazydreamer@gmail.com>
2019-08-06 03:17:58 +0530

    ^first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+
+# html-compressor
+
+[![npm version](https://img.shields.io/npm/v/html-compressor.svg?style=flat-square)](https://www.npmjs.org/package/html-compressor)
+[![npm license](https://img.shields.io/static/v1.svg?label=License&message=MIT&color=informational)](https://github.com/nepsho/html-compressor/blob/master/LICENSE)
+[![npm repository](https://img.shields.io/static/v1.svg?label=Repository&message=GitHub&color=yellow)](https://github.com/nepsho/html-compressor)
+[![npm author](https://img.shields.io/static/v1.svg?label=Author&message=bcrazydreamer&color=success)](https://www.npmjs.com/~bcrazydreamer)
+
+## Installing
+[![NPM](https://nodei.co/npm/html-compressor.png?mini=true)](https://www.npmjs.org/package/html-compressor)
+
+**Using npm:**
+```bash
+$ npm install html-compressor
+```
+
+**Using bower:**
+
+```bash
+$ bower install html-compressor
+```
+
+**Using yarn:**
+
+```bash
+$ yarn add html-compressor
+```
+
+## Examples
+
+```js
+var express = require('express');
+var htmlCompressor = require("html-compressor");
+```
+```js
+var app = express();
+app.use(htmlCompressor());
+```
+## licence
+MIT [licence](https://opensource.org/licenses/MIT)
+
+## Author
+@BCrazyDreamer
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+var minify = require('html-minifier').minify;
+/* Bvalid Code */
+function isBoolean(variable) { return Object.prototype.toString.call(variable) === "[object Boolean]"}
+function isObject(variable) { return Object.prototype.toString.call(variable) === "[object Object]"}
+
+module.exports = function compress_html(_data) {
+  var data = {
+    removeComments: true,
+    collapseWhitespace: true,
+    collapseBooleanAttributes: true,
+    removeAttributeQuotes: true,
+    removeEmptyAttributes: true
+  }
+  if(isObject(_data)){
+    if(isBoolean(_data.js)){
+      data.minifyJS = _data.js;
+    }
+    if(isBoolean(_data.css)){
+      data.minifyCSS = _data.js;
+    }
+    if(isBoolean(_data.comments)){
+      data.minifyCSS = _data.comments;
+    }
+    if(isBoolean(_data.whitespace)){
+      data.minifyCSS = _data.whitespace;
+    }
+  }
+  function compressor(req, res, next) {
+    var skip = false;
+    var sendCompressHtml = function (callback) {
+      if (typeof callback === 'undefined') {
+        return function (err, html) {
+          if (err) {
+            return next(err);
+          }
+          html = minify(html, data);
+          res.send(html);
+        }
+      } else {
+        return function (err, html) {
+          if (html) {
+            html = minify(html, data);
+          }
+          callback(err, html);
+        }
+      }
+    };
+    res.oldRender = res.render;
+    res.render = function (view, renderOpts, callback) {
+      this.oldRender(view, renderOpts, sendCompressHtml(callback));
+    };
+    return next();
+  }
+  return (compressor);
+}
