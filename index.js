68f728fee54135ff054027b658e71a6ed2f726ec
BCrazyDreamer <bcrazydreamer@gmail.com>
2019-08-06 12:17:00 +0530

    ^

--- index.js
+++ index.js
@@ -1,56 +1,100 @@
 'use strict';
-var minify = require('html-minifier').minify;
-/* Bvalid Code */
-function isBoolean(variable) { return Object.prototype.toString.call(variable) === "[object Boolean]"}
-function isObject(variable) { return Object.prototype.toString.call(variable) === "[object Object]"}
+const minify = require('html-minifier').minify;
 
-module.exports = function compress_html(_data) {
-  var data = {
+/* Bvalid Functions*/
+const isBoolean = function(variable) { return Object.prototype.toString.call(variable) === "[object Boolean]"}
+const isObject = function(variable) { return Object.prototype.toString.call(variable) === "[object Object]"}
+const isFunction = function(variable) {return Object.prototype.toString.call(variable) === "[object Function]"}
+/*-----------------*/
+
+const errorMsgHead = '\u001B[38;2;255;0;0mhtml-compressor\u001B[39m-';
+
+const optionFilter = function(_data){
+  var _payload = {
     removeComments: true,
     collapseWhitespace: true,
-    collapseBooleanAttributes: true,
+    removeEmptyAttributes: true,
     removeAttributeQuotes: true,
-    removeEmptyAttributes: true
+    collapseBooleanAttributes: true
   }
   if(isObject(_data)){
     if(isBoolean(_data.js)){
-      data.minifyJS = _data.js;
+      _payload.minifyJS = _data.js;
     }
     if(isBoolean(_data.css)){
-      data.minifyCSS = _data.css;
+      _payload.minifyCSS = _data.css;
     }
     if(isBoolean(_data.comments)){
-      data.minifyCSS = _data.comments;
+      _payload.minifyCSS = _data.comments;
     }
     if(isBoolean(_data.whitespace)){
-      data.minifyCSS = _data.whitespace;
+      _payload.minifyCSS = _data.whitespace;
     }
   }
-  function compressor(req, res, next) {
-    var skip = false;
-    var sendCompressHtml = function (callback) {
-      if (typeof callback === 'undefined') {
-        return function (err, html) {
-          if (err) {
-            return next(err);
-          }
-          html = minify(html, data);
-          res.send(html);
-        }
-      } else {
-        return function (err, html) {
-          if (html) {
-            html = minify(html, data);
-          }
-          callback(err, html);
-        }
-      }
-    };
-    res.oldRender = res.render;
-    res.render = function (view, renderOpts, callback) {
-      this.oldRender(view, renderOpts, sendCompressHtml(callback));
-    };
-    return next();
-  }
-  return (compressor);
+  return _payload;
 }
+
+const compressor = function(req, res, next, data, isRenderModify,customRender){
+  var sendCompressHtml = function (cb) {
+    if(isFunction(cb)) {
+      return function (err, html) {
+        if (err) {
+          return next(err);
+        }
+        if (html) {
+          try{
+            html = minify(html, data);
+          } catch(err){
+            console.log(errorMsgHead+'Unable to compress html');
+          }
+        }
+        return cb(err, html);
+      }
+    } else {
+      return function (err, html) {
+        if (err) {
+          return next(err);
+        }
+        try{
+          html = minify(html, data);
+        } catch(err){
+          console.log(errorMsgHead+'Unable to compress html');
+        }
+        return res.send(html);
+      }
+    }
+  }
+  if(isRenderModify){
+    res._tempRender = res.render;
+    res.render = function (view, r_opts, cb2) {
+      this._tempRender(view, r_opts, sendCompressHtml(cb2));
+    }
+  }
+  if(customRender){
+    res[customRender] = function (view, r_opts, cb2) {
+      this.render(view, r_opts, sendCompressHtml(cb2));
+    }
+  }
+  res.renderCompress = function (view, r_opts, cb2) {
+    this.render(view, r_opts, sendCompressHtml(cb2));
+  }
+  return next();
+}
+
+/* compress engine */
+module.exports = function(_data){
+  var data = optionFilter(_data);
+  var isRenderModify = isBoolean(_data.render) ? _data.render : true;
+  var customRender = (
+    typeof _data.custom === 'string' &&
+    _data.custom.trim().length !== 0
+  ) ? _data.custom : null;
+
+  return function(req, res, next){
+    return compressor(
+      req, res, next,
+      data,isRenderModify,customRender
+    );
+  }
+}
+/*-----------------*/
